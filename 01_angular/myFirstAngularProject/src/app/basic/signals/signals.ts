2c98e9f6f64638c3b7292ff7cb2db1e1348c6362
NICOLAU Bastien <nicolau.bastien@proton.me>
2026-04-14 09:58:24 +0200

    feat(angular): illustrate signal reactivity with setTimeout in signals component

--- 01_angular/myFirstAngularProject/src/app/basic/signals/signals.ts
+++ 01_angular/myFirstAngularProject/src/app/basic/signals/signals.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signals',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signals.html',
   styleUrl: './signals.scss',
 })
-export class Signals {}
+export class Signals {
+  protected count1: number = 10;
+  protected count2: WritableSignal<number> = signal(10);
+
+  constructor() {
+    setTimeout(() => (this.count1 = 30), 3000);
+    setTimeout(() => this.count2.set(30), 3000);
+  }
+}

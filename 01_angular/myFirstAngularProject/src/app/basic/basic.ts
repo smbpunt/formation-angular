74c4a382b1407b9069d0043f7ba2f48d08a00ee9
NICOLAU Bastien <nicolau.bastien@proton.me>
2026-04-14 09:31:19 +0200

    feat(angular): add signals component and component selector in basic
    Add a select-based component switcher in basic using @switch, and
    scaffold a new signals component. Disable Angular CLI analytics.

--- 01_angular/myFirstAngularProject/src/app/basic/basic.ts
+++ 01_angular/myFirstAngularProject/src/app/basic/basic.ts
@@ -4,18 +4,20 @@ import { Calculatrice } from './calculatrice/calculatrice';
 import { FormsModule } from '@angular/forms';
 import { Produits } from './produits/produits';
 import { Tva } from './tva/tva';
+import { Signals } from './signals/signals';
 
 @Component({
   selector: 'app-basic',
-  imports: [Couleur, Calculatrice, Produits, Tva, FormsModule],
+  imports: [Couleur, Calculatrice, Produits, Tva, FormsModule, Signals],
   templateUrl: './basic.html',
   styleUrl: './basic.scss',
 })
 export class Basic {
   message: string = 'Bonjour AMU';
   colorMessage: string = 'red';
-
   username: string = '';
+  protected composants: string[] = ['Couleur', 'Calculatrice', 'Produits', 'TVA', 'Signals'];
+  protected selectedComposant: string = this.composants[0];
 
   protected onAction(): void {
     this.message = `Bonjour ${this.username} !`;

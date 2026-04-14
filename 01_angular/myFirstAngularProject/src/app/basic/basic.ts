d051611131c9564e3a8b0155ef8f8ef942d97d21
NICOLAU Bastien <bastien.nicolau@univ-amu.fr>
2026-04-14 08:47:45 +0200

    feat(angular): wire new components into basic and add ngModel demo

--- 01_angular/myFirstAngularProject/src/app/basic/basic.ts
+++ 01_angular/myFirstAngularProject/src/app/basic/basic.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Couleur } from './couleur/couleur';
+import { Calculatrice } from './calculatrice/calculatrice';
+import { FormsModule } from '@angular/forms';
+import { Produits } from './produits/produits';
+import { Tva } from './tva/tva';
 
 @Component({
   selector: 'app-basic',
-  imports: [],
+  imports: [Couleur, Calculatrice, Produits, Tva, FormsModule],
   templateUrl: './basic.html',
   styleUrl: './basic.scss',
 })
-export class Basic {}
+export class Basic {
+  message: string = 'Bonjour AMU';
+  colorMessage: string = 'red';
+
+  username: string = '';
+
+  protected onAction(): void {
+    this.message = `Bonjour ${this.username} !`;
+    this.colorMessage = this.username.length > 5 ? 'green' : 'blue';
+  }
+}

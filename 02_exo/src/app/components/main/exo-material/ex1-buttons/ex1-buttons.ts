41317c01875e954998a3f4d00120901aaa8357ed
NICOLAU Bastien <nicolau.bastien@proton.me>
2026-04-16 14:43:55 +0200

    feat(ex1-buttons): complete exo 1 — button variants with click counter

--- 02_exo/src/app/components/main/exo-material/ex1-buttons/ex1-buttons.ts
+++ 02_exo/src/app/components/main/exo-material/ex1-buttons/ex1-buttons.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-ex1-buttons',
-  imports: [],
+  imports: [MatButton],
   templateUrl: './ex1-buttons.html',
   styleUrl: './ex1-buttons.scss',
 })
-export class Ex1Buttons {}
+export class Ex1Buttons {
+  protected nbClic: number = 0;
+
+  protected increment() {
+    this.nbClic++;
+  }
+}
